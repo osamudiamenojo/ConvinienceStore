61afa07d1b31906060a8617d4b1ce17df95d0187
Ojo Victor <osamudiamenojo@users.noreply.github.com>
2022-10-13 16:25:17 +0100

    initial submission

--- StoreApp/src/main/java/service/impl/CashierServiceImpl.java
+++ StoreApp/src/main/java/service/impl/CashierServiceImpl.java
@@ -3,6 +3,7 @@ package service.impl;
 import entities.Customer;
 import entities.Product;
 import entities.Staff;
+import enums.Role;
 import exceptions.ServiceException;
 import service.CashierService;
 import storerepository.Store;
@@ -11,8 +12,25 @@ public class CashierServiceImpl implements CashierService {
     Store store = new Store();
     public String sellProduct(Staff cashier, Customer customer) {
         if (customer == null || cashier == null)throw new ServiceException("customer or cashier cannot be null");
+        if(Role.CASHIER.equals(cashier.getRole())) {
+            customer = store.getCustomerQueue().poll();
+            if(store.getProductsInStore().contains(customer.getProductToBuy())) {
+                for (Product productAvailable : store.getProductsInStore()
+                ) {
+
+                }
+            }else {
+                return "Product unavailable";
+            }
+        } else {
+            return "You need to be a cashier to sell";
+        }
+
+
         return "Queue is empty";
-    }
+        }
+
+
 
     public String dispenseReceipt(Staff cashier, Customer customer) {
         if(customer==null) throw new ServiceException("customer cannot be null");
