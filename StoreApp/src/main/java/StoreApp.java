974dd63634e36791b1bed2bb32d49d0735089d2c
Ojo Victor <osamudiamenojo@users.noreply.github.com>
2022-10-14 09:11:22 +0100

    initial submission

--- StoreApp/src/main/java/StoreApp.java
+++ StoreApp/src/main/java/StoreApp.java
@@ -8,6 +8,7 @@ import enums.Qualification;
 import enums.Role;
 import service.impl.CashierServiceImpl;
 import service.impl.CustomerServiceImpl;
+import service.impl.ManagerServiceImpl;
 import storerepository.Store;
 
 import java.util.UUID;
@@ -15,6 +16,7 @@ import java.util.UUID;
 public class StoreApp {
     private final static CashierServiceImpl cashierService = new CashierServiceImpl();
     private final static CustomerServiceImpl customerService= new CustomerServiceImpl();
+    private final static ManagerServiceImpl managerService = new ManagerServiceImpl();
     private final static Store store =new Store();
     public static void main(String[] args) {
 
@@ -29,9 +31,18 @@ public class StoreApp {
         Product product3= new Product("phone charger", "P13", 800.00, 100, Category.HYGIENE);
 
 
-        Customer customer = new Customer("Adedotun", Gender.MALE, 200005.00, product1, 45);
+        Customer customer1 = new Customer("Adedotun", Gender.MALE, 20005.00, product1, 45);
+        Customer customer2 = new Customer("Samuel", Gender.MALE, 200005.00, product1, 40);
+        Customer customer3 = new Customer("Zechariah", Gender.MALE, 2005.00, product1, 20);
+        Customer customer4 = new Customer("Abednego", Gender.MALE, 200005.00, product1, 5);
+        Customer customer5 = new Customer("Michael", Gender.MALE, 67005.00, product1, 45);
+        Customer customer6 = new Customer("Emmanuel", Gender.MALE, 25000.00, product1, 45);
 
         System.out.println(applicant1.apply());
+        String hireResult = managerService.hire(manager, applicant1);
+        System.out.println(hireResult);
+        String buyingResult = customerService.buyProduct(customer1, product1);
+        System.out.println(buyingResult);
 
     }
 
